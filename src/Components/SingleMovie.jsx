668862d42c8dbc8b6eaeec43f6f5c17311b74840
Rohit Gulam <rohitsgulam@gmail.com>
2022-05-25 21:26:22 +0300

    created single movie container

---
+++ src/Components/SingleMovie.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {AiTwotoneStar} from 'react-icons/ai'
+
+function SingleMovie( {img, movieName,  cat, rating} ) {
+  return (
+    <div>
+        <div className="h-[420px] rounded flex flex-col justify-between">
+            <img className='h-[360px] rounded-3xl' src={img} alt={movieName} />
+
+            <div className="flex justify-between">
+                <div className="name-cat">
+                    <h3 className='text-xl font-semibold'>{movieName}</h3>
+                    <p>{cat}</p>
+                </div>
+                <div className="rating flex text-gray-500">
+                    <AiTwotoneStar/> {rating}
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SingleMovie
