7b8468808702b6e1b3fb63c553c3e1c9abc4c931
Rohit Gulam <rohitsgulam@gmail.com>
2022-06-04 15:39:55 +0300

    created a base img url and sizes

--- src/API/index.js
+++ src/API/index.js
@@ -1,4 +1,16 @@
 export const GETBANNER =
   "https://api.themoviedb.org/3/movie/550/images?api_key=";
 export const SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=`;
-export const IMAGE_API = `https://image.tmdb.org/t/p/w185`
+export const IMAGE_API = `https://image.tmdb.org/t/p/`;
+export const imageSize = {
+  0:"w92",
+  1:"w154",
+  2:"w185",
+  3:"w342",
+  4:"w500",
+  5:"w780",
+  6:"original"
+};
+
+
+
