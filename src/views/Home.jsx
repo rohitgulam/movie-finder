c634e9512596c3341bc485594419baf0edc071f7
Rohit Gulam <rohitsgulam@gmail.com>
2022-06-13 15:46:43 +0300

    added popular section to home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,12 +3,14 @@ import Navigation from "../Components/Navigation";
 import Recommended from "../pages/Recommended";
 import MostRated from "../pages/MostRated";
 import SearchBar from "../Components/SearchBar";
+import Popular from "../pages/Popular";
 
 function Home() {
   return (
     <>
       <Recommended />
       <MostRated />
+      <Popular />
     </>
   );
 }
